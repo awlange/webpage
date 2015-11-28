1660193dd2bf0916d85d851c37e74dfb6ee2cb25
Adrian Lange <alange@apple.com>
2015-11-28 12:41:27 -0800

    lazy load images

--- js/site.js
+++ js/site.js
@@ -1,3 +1,7 @@
+var images = {
+  "thumbnail-ade": "img/ade2orbital.png"
+};
+
 $(document).ready(function() {
 
     // Variables
@@ -34,12 +38,39 @@ $(document).ready(function() {
     }
 
     function onScroll() {
+        // Navbar docking
         if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
             $body.addClass('has-docked-nav');
         }
         if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
             $body.removeClass('has-docked-nav');
         }
+
+        // Lazy loading images
+        $(".thumbnail-wrapper").filter(".not-loaded").filter(function(i, el) {
+            return elementInViewport(el);
+        }).each(function(i, el) {
+            // Let image be on screen for a minimum amount of time before loading
+            var $this = $(this);
+            setTimeout(function() {
+                // Still on screen
+                if (elementInViewport(el)) {
+                    var $spinner = $this.children("div");
+                    var $img = $this.children("img");
+                    $spinner.addClass("hidden");
+                    $img.attr("src", images[$img.attr("id")]).fadeIn("slow").removeClass("hidden");
+                    $this.removeClass("not-loaded");
+                }
+            }, 500);
+        });
+
+
+    }
+
+    function elementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return rect.top >= 0 && rect.left >= 0
+            && rect.top <= (window.innerHeight || document.documentElement.clientHeight);
     }
 
     init();
